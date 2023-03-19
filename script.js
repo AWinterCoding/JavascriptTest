151cd5b4cd13d252777c83f6712e7bac5f14f86d
AWinter <thumbspurcell@gmail+universityofutah.com>
2023-03-19 14:58:49 -0600

    added comments for readability

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var score = 0;
 //note this number will always be less than the display for what question you are on.
 var questionCounter = 0;
 
+//potential questions, add more if needed
 var questions = [
   "Which one of these is NOT the primitive?",
   "How do you send things to the console?",
@@ -12,18 +13,22 @@ var questions = [
   "What is the rel of the link element that style.css utilizes",
 ];
 
+//all of these options will be displayed in the buttons. Don't include more than 4 selections per array
 var options = [
   ["object", "string", "number", "null"],
   ["document.log", "console.log", "alert", "query.sendID"],
   ["page", "selector", "document", "var"],
   ["script", "stylesheet", "index.html", "cascading style sheets"],
 ];
+
+//all answers to questions
 var answers = ["object", "console.log", "document", "stylesheet"];
 
 // query Selector works better for grabbing elements https://stackoverflow.com/questions/70910043/javascript-grabbing-button-inside-div-with-id
 // Learned that today ^-^
 init();
 
+//TODO initialize function, however I will be moving this to a "start game" since we shouldn't be reinitializing
 function init() {
   refresh();
   if (questionCounter < questions.length) {
@@ -38,20 +43,24 @@ function init() {
   }
 }
 
+//this calls both the question populate and choices populate functions to stop repeating
 function populate(choices, question) {
   questionPopulate(question);
   choicesPopulate(choices);
 }
 
+//updates both the score and the timer with penalties
 function refresh() {
   document.querySelector("#score").textContent = score;
   document.querySelector("#timer").textContent = count;
 }
 
+//populates the leading question
 function questionPopulate(question) {
   question.innerHTML = questions[questionCounter];
 }
 
+//populates all choices
 function choicesPopulate(children) {
   console.log(children);
   for (i = 0; i < children.length; i++) {
@@ -60,6 +69,8 @@ function choicesPopulate(children) {
 }
 
 //source for more explaination on this https://stackoverflow.com/questions/34896106/attach-event-to-dynamic-elements-in-javascript
+
+//universal button clicker function
 document.addEventListener("click", function (event) {
   const target = event.target.closest("button");
   if (target == null) {
@@ -68,6 +79,8 @@ document.addEventListener("click", function (event) {
     answerCheck(target.innerHTML);
   }
 });
+
+//answer checker for each answer
 function answerCheck(targetHTML) {
   if (targetHTML == answers[questionCounter]) {
     questionCounter++;
